393eba3bcdab9bab31b2ce231789673f911c4c58
PEAC337 <k.j@peaceoutpost.icu>
2025-01-22 06:15:47 -0600

    commit to main

---
+++ Uniswap-V3-Periphery/contracts/test/PoolTicksCounterTest.sol
@@ -0,0 +1,33 @@
+/*
+    
+   ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██    ██████  ███████ ██    ██
+  ██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██      ██    ██
+  ██      ██    ██ ██    ██ █████   ██████  ██    ██ ██    ██ █████      ██   ██ █████   ██    ██
+  ██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██       ██  ██
+   ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██ ██ ██████  ███████   ████
+  
+  Find any smart contract, and build your project faster: https://www.cookbook.dev
+  Twitter: https://twitter.com/cookbook_dev
+  Discord: https://discord.gg/cookbookdev
+  
+  Find this contract on Cookbook: https://www.cookbook.dev/contracts/Uniswap-V3-Periphery/?utm=code
+  */
+  
+  // SPDX-License-Identifier: GPL-2.0-or-later
+import 'Uniswap-V3-Periphery/@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
+
+pragma solidity >=0.6.0;
+
+import '../libraries/PoolTicksCounter.sol';
+
+contract PoolTicksCounterTest {
+    using PoolTicksCounter for IUniswapV3Pool;
+
+    function countInitializedTicksCrossed(
+        IUniswapV3Pool pool,
+        int24 tickBefore,
+        int24 tickAfter
+    ) external view returns (uint32 initializedTicksCrossed) {
+        return pool.countInitializedTicksCrossed(tickBefore, tickAfter);
+    }
+}
